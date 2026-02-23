327e684c2f6b6e1e0db2e2de27b562b6e799c6a7
Rajatkashyap <rajatkashyap6822@gmail.com>
2026-02-23 15:56:44 +0530

    Snt

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 let blinker = document.querySelector('#blinker')
 let cursor = document.querySelector('.cursor')
 let images = document.querySelectorAll('.images img')
+let buttons = document.querySelectorAll('.images .btn')
 
-document.addEventListener('mousemove',function(dets){
-    console.log(dets.clientX, dets.clientY)
+function cursorMove(){
+    document.addEventListener('mousemove',function(dets){
     cursor.style.left = dets.clientX+'px'
     cursor.style.top = dets.clientY+'px'
 })
-
+}
+cursorMove()
+ 
 
 function blink() {
     setTimeout(function () {
@@ -17,7 +20,25 @@ function blink() {
     }, 1000)
 
     setInterval(function () {
-        blinker.style.boxShadow = '0 0 15px white'
+        blinker.style.boxShadow = '0 0 20px white'
     }, 2000)
 }
 blink()
+
+function imageButton(){
+    buttons.forEach(function(btn) {
+    let upper = btn.querySelector('.images .btn .upper')
+    let downer = btn.querySelector('.images .btn .downer')
+
+    btn.addEventListener('mouseenter',function(){
+        upper.style.transform = 'translate(120%, -120%)'
+        downer.style.transform = 'translate(0%, 0%)'
+        images.style.filter = 'grayscale(0%)'
+    })
+    btn.addEventListener('mouseleave',function(){
+        upper.style.transform = 'translate(0%, 0%)'
+        downer.style.transform = 'translate(-120%, 120%)'
+    })
+});
+}
+imageButton()
